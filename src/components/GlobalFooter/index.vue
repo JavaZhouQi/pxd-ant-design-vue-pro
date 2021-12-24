0cf2f42c2d66944b22bf3dc02bf7678a1c60fa42
zhouqi <javazhouqi@163.com>
2021-12-24 16:59:27 +0800

    init

---
+++ src/components/GlobalFooter/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <global-footer class="footer custom-render">
+    <template v-slot:links>
+      <a href="https://www.github.com/vueComponent/pro-layout" target="_blank">Pro Layout Github</a>
+      <a href="https://www.github.com/vueComponent/ant-design-vue-pro" target="_blank">ant-design-vue-pro Github</a>
+      <a href="https://www.github.com/sendya/" target="_blank">@Sendya</a>
+      <a href="https://www.github.com/javazhouqi/" target="_blank">@PiXiaoDan</a>
+      <a href="https://github.com/vueComponent" target="_blank">vueComponent</a>
+    </template>
+  </global-footer>
+</template>
+
+<script>
+import { GlobalFooter } from '@ant-design-vue/pro-layout'
+
+export default {
+  name: 'ProGlobalFooter',
+  components: {
+    GlobalFooter
+  }
+}
+</script>
